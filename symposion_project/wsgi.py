9928da33eb77b5280f6d7d59099a4f9360f33d81
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2013-02-04 11:21:22 +0100

    Re-applied lost wsgi setting.

--- symposion_project/wsgi.py
+++ symposion_project/wsgi.py
@@ -1,4 +1,9 @@
 import os
+import sys
+sys.path.append("/web/confenv/symposion")
+
+activate_this = '/web/confenv/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion_project.settings")
 
