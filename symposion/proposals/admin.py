908ad6293c0e38ea0efe3dcbea5ef4e745086619
Pierre de Buyl <pdebuyl@nononospam.seriously.ulb.ac.be>
2013-05-06 09:16:39 +0200

    Enabled custom admin display for proposals.

--- symposion/proposals/admin.py
+++ symposion/proposals/admin.py
@@ -1,31 +1,30 @@
 from django.contrib import admin
 
-# from symposion.proposals.actions import export_as_csv_action
-from symposion.proposals.models import ProposalSection, ProposalKind
+from symposion.proposals.actions import export_as_csv_action
+from symposion.proposals.models import ProposalSection, ProposalKind, ProposalBase
 
 
-# admin.site.register(Proposal,
-#     list_display = [
-#         "id",
-#         "title",
-#         "speaker",
-#         "speaker_email",
-#         "kind",
-#         "audience_level",
-#         "cancelled",
-#     ],
-#     list_filter = [
-#         "kind__name",
-#         "result__accepted",
-#     ],
-#     actions = [export_as_csv_action("CSV Export", fields=[
-#         "id",
-#         "title",
-#         "speaker",
-#         "speaker_email",
-#         "kind",
-#     ])]
-# )
+admin.site.register(ProposalBase,
+    list_display = [
+        "id",
+        "title",
+        "speaker",
+        "speaker_email",
+        "kind",
+        "cancelled",
+    ],
+    list_filter = [
+        "kind__name",
+        "result__accepted",
+    ],
+    actions = [export_as_csv_action("CSV Export", fields=[
+        "id",
+        "title",
+        "speaker",
+        "speaker_email",
+        "kind",
+    ])]
+)
 
 
 admin.site.register(ProposalSection)
